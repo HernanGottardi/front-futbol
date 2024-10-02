c5d04760861f21620f13e245d3f84c932d9b6b24
Hernan <108548653+HernanGottardi@users.noreply.github.com>
2024-10-01 22:04:45 -0300

    4

--- database.js
+++ database.js
@@ -1,7 +1,8 @@
 // Agregar un documento con datos personalizados
 export async function getGames(newNameDoc, objValor) {
   const response = await fetch('https://api-futbol-dywbxph5q-hernans-projects-b8ac9745.vercel.app/games', {
-    method: 'GET', // Método POST
+    method: 'GET', 
+    mode: 'cors',// Método POST
   })
     .then(response => response.json()) // Convertir la respuesta a JSON
     .then(data => {
